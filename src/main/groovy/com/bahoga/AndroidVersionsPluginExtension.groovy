874c37235a3d7d0927472b85d9deea8da6aad0a2
Lucas Bais <lucas.bais@gmail.com>
2017-12-02 19:41:37 -0300

    versions upgrade

--- src/main/groovy/com/bahoga/AndroidVersionsPluginExtension.groovy
+++ src/main/groovy/com/bahoga/AndroidVersionsPluginExtension.groovy
@@ -1,22 +1,20 @@
 package com.bahoga
 
 class AndroidVersionsPluginExtension {
-    Integer androidCompileSdkVersion = 26
-    Integer androidTargetSdkVersion = 26
-    String androidBuildToolsVersion = '26.0.2'
+    Integer androidCompileSdkVersion = 27
+    Integer androidTargetSdkVersion = 27
+    String androidBuildToolsVersion = '26.1.1'
     Integer androidMinSdkVersion = 15
-    String androidAppCompatVersion = '26.1.0'
+    String androidAppCompatVersion = '27.0.2'
     String androidGooglePlayServicesVersion = '10.0.0'
     String retrofitVersion = '2.2.0'
     String facebookSDKVersion = '4.21.1'
-    String urbanAirshipSDKVersion = '7.2.5'
     String daggerVersion = '2.9'
     String butterknifeVersion = '8.5.1'
     String reactiveAndroidVersion = '2.0.1'
     String reactiveJavaVersion = '2.1.0'
     String javaxInjectVersion = '1'
     String androidConstraintLayoutVersion = '1.0.2'
-    String robotoTextViewVersion = '3.0.0'
     String okHttpVersion = '3.6.0'
     String universalLoaderVersion = '1.9.4'
 }
